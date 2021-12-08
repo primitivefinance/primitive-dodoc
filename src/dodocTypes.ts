f44aab958d9e703023130da0ec7588d982946f92
Clement Lakhal <clementlakhal@gmail.com>
2021-12-08 16:40:10 +0400

    feat: update core source

--- src/dodocTypes.ts
+++ src/dodocTypes.ts
@@ -1,12 +1,13 @@
 import { CompilerOutputContract } from 'hardhat/types';
 
-interface ErrorDevdocArrayItem {
+export interface ErrorDevdocArrayItem {
   details?: string;
   params?: {
     [key: string]: string;
   }
 }
-interface ErrorUserdocArrayItem {
+
+declare interface ErrorUserdocArrayItem {
   notice?: string;
 }
 
@@ -45,6 +46,17 @@ export interface CompilerOutputContractWithDocumentation extends CompilerOutputC
         }
       }
     }
+    stateVariables?: {
+      [key: string]: {
+        details?: string;
+        params: {
+          [key: string]: string;
+        }
+        returns: {
+          [key: string]: string;
+        }
+      }
+    }
   },
   userdoc?: {
     errors?: {
@@ -64,76 +76,6 @@ export interface CompilerOutputContractWithDocumentation extends CompilerOutputC
   }
 }
 
-export interface Error {
-  sig: string;
-  code?: string;
-  notice?: string;
-  details?: string;
-  params?: {
-    [key: string]: {
-      description: string;
-      type: string;
-    }
-  }
-}
-
-export interface Errors {
-  [key: string]: Error;
-}
-
-export interface Event {
-  sig: string;
-  code?: string;
-  notice?: string;
-  details?: string;
-  params?: {
-    [key: string]: {
-      type?: string;
-      indexed: boolean;
-      description?: string;
-    }
-  }
-}
-
-export interface Events {
-  [key: string]: Event;
-}
-
-export interface Method {
-  sig: string;
-  code?: string;
-  stateMutability?: string;
-  notice?: string;
-  details?: string;
-  params?: {
-    [key: string]: {
-      type?: string;
-      description?: string;
-    }
-  };
-  returns?: {
-    [key: string]: {
-      type?: string;
-      description?: string;
-    }
-  };
-}
-
-export interface Methods {
-  [key: string]: Method;
-}
-
-export interface ContractDocumentation {
-  name: string;
-  title?: string;
-  author?: string;
-  notice?: string;
-  details?: string;
-  methods?: Methods;
-  events?: Events;
-  errors?: Errors;
-}
-
 export interface AbiElementPut {
   internalType: string;
   name: string;
@@ -142,9 +84,63 @@ export interface AbiElementPut {
 }
 
 export interface AbiElement {
-  type: 'function' | 'event' | 'error';
+  type: 'constructor' | 'function' | 'event' | 'error';
   name: string;
   stateMutability?: string;
-  inputs?: AbiElementPut[];
-  outputs?: AbiElementPut[];
+  inputs: AbiElementPut[];
+  outputs: AbiElementPut[];
+}
+
+export interface Param {
+  type?: string;
+  description?: string;
+  indexed?: boolean;
+}
+
+export interface Method {
+  code?: string;
+  stateMutability?: string;
+  notice?: string;
+  details?: string;
+  inputs: {
+    [key: string]: Param;
+  }
+  outputs: {
+    [key: string]: Param;
+  }
+}
+
+export interface Event {
+  code?: string;
+  notice?: string;
+  details?: string;
+  inputs: {
+    [key: string]: Param;
+  }
+}
+
+export interface Error {
+  code?: string;
+  description?: string;
+  details?: string;
+  inputs: {
+    [key: string]: Param;
+  }
+}
+
+export interface Doc {
+  name?: string;
+  title?: string;
+  notice?: string;
+  details?: string;
+  author?: string;
+  methods: {
+    [key: string]: Method;
+  }
+  events: {
+    [key: string]: Event;
+  }
+  errors: {
+    [key: string]: Event;
+  }
 }
